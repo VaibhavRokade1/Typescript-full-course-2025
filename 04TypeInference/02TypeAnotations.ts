1badf083deccb747aba2eb677a655a51850b07d6
VaibhavRokade1 <rokadevaibhv04@gmail.com>
2025-10-14 15:19:07 +0530

    what is anotations

---
+++ 04TypeInference/02TypeAnotations.ts
@@ -0,0 +1,31 @@
+// Type Anotations
+
+// the annotations  in typescript is the practice of explicitly specifying types of variable , function,
+// and pramiters.
+// Ex.
+
+// const a: string  = "Vaibhav Rokade";
+
+// console.log(a);
+// let num  : number= 48957;
+
+// Error : syntax error --->
+// let Obj: { string; number; boolean } = { name: "", age: 21, isLoggedIn: true };
+
+// object Anontations
+// let user: { name: string; age: number; isLoggedIn: boolean } = {
+//   name: "Vaibhav Rokade",
+//   age: 21,
+//   isLoggedIn: true,
+// };
+
+// console.log(user);
+
+// type anotations on functions and parameters
+
+// const myFunction  = (name : string , age : number) : any =>
+//     {
+//         console.log(name , age)
+//     }
+
+//     myFunction("Vaibhav Rokade" , 21);
