167ef4897453c8ce502b80b5cb31cca5bee679b6
VaibhavRokade1 <rokadevaibhv04@gmail.com>
2025-12-07 01:28:07 +0530

    ts anotations and inference in ts

--- 03.TypeAnotations&Inference.js
+++ 03.TypeAnotations&Inference.js
@@ -1,6 +1,15 @@
-var Str = "Vaibhav";
-console.log(Str);
+// const Str: string = "Vaibhav";
+// console.log(Str);
 // --------------------------
-var ChaiName = "Chai Our Code";
-ChaiName = "Vaibhav";
+// let ChaiName: string = "Chai Our Code";
+// ChaiName = "Vaibhav";
 // ChaiName = 30; we got an errors becouse of type is miss match
+// Annotation --> In Annotation we explicitly define our datatypes
+var age = 25;
+var name1 = "John";
+// console.log(name1);
+var Num1 = true ? " " : 1; // explicitly we define type of that Particuler variable
+// Inference --> In Inference TypeScript automatically detects the datatype
+var city = "London"; // inferred as string
+var count = 10; // inferred as number
+var Num2 = true ? Math.random() * 20 + 1 : "1"; // inferred as string | number
--- 03.TypeAnotations&Inference.ts
+++ 03.TypeAnotations&Inference.ts
@@ -10,9 +10,12 @@
 // Annotation --> In Annotation we explicitly define our datatypes
 let age: number = 25;
 let name1: string = "John";
+// console.log(name1);
 
-console.log(name1);
+let Num1: string | number = true ? " " : 1; // explicitly we define type of that Particuler variable
 
 // Inference --> In Inference TypeScript automatically detects the datatype
 let city = "London"; // inferred as string
 let count = 10; // inferred as number
+
+let Num2 = true ? Math.random() * 20 + 1 : "1"; // inferred as string | number
